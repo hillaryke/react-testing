c69a7099c0cb7e60b2ee33f59e3da7c3aae684a5
hillaryke <tenskap@gmail.com>
2023-07-23 18:57:37 +0300

    use it instead of test

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -5,13 +5,13 @@ import { render, screen } from '@testing-library/react';
 import { Greet } from './Greet';
 
 describe('Greet', () => {
-    test('renders correctly', () => {
+    it('renders correctly', () => {
         render(<Greet />)
         const textElement = screen.getByText('Hello')
         expect(textElement).toBeInTheDocument()
     })
 
-    test('renders a correctly with a name', () => {
+    it('renders a correctly with a name', () => {
         render(<Greet name={"John"}/>)
         const textElement = screen.getByText('Hello John')
         expect(textElement).toBeInTheDocument()
